b4a4286308c7c608c32e44fb7894e80ff47b8fc1
muzyukins <muzyukins@gmail.com>
2014-12-02 19:22:13 +0200

    [FIX]routes resources posts, comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Sernobi::Application.routes.draw do
   get "welcome/index"
   get "welcome/about"
 
-  root to: 'welcome#index'
+  root to: "welcome#index"
+
+  resources :comments
+  resources :posts
 end
