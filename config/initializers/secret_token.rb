4154571f966fe94009611e4b4b3eb5799674097c
muzyukins <muzyukins@gmail.com>
2014-12-02 14:51:06 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sernobi::Application.config.secret_key_base = '11d46a4dec1b9d7412edc8dac1ac513803295cbb9ca23eab170c4de46209f3a6e6c9db9c228b92ed03a738f854e923443da41f23fabbd9517acd837c05f63cd4'
